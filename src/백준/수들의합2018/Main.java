3fb3c2d368e38a0a00d17b0dc23214121309e1fc
samkimuel <samkim2626@gmail.com>
2022-01-13 22:32:41 +0900

    투포인터 - 백준 2018

---
+++ src/백준/수들의합2018/Main.java
@@ -0,0 +1,93 @@
+package 백준.수들의합2018;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static FastReader scan = new FastReader();
+	static StringBuilder sb = new StringBuilder();
+
+	static int N;
+
+	static void input() {
+		N = scan.nextInt();
+	}
+
+	static void solution() {
+		int R = 1;
+		int count = 0;
+		int sum = 0;
+
+		for (int L = 1; L <= N; L++) {
+			while (sum < N && R <= N) {
+				sum += R;
+				R++;
+			}
+
+			if (sum == N) {
+				count++;
+			}
+			sum -= L;
+		}
+
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		input();
+		solution();
+	}
+
+	static class FastReader {
+
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
